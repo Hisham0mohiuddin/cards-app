4f25227f84935970512e278ebab2a01f209f6796
Hisham0mohiuddin <hishamjuly@gmail.com>
2026-06-13 19:16:57 +0300

    added the top bar without tracker states

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,10 @@
+import '../styles/TopBar.css'
+export default function TopBar() {
+    return (
+        <div className="TopBar">
+            <h1>Pokemon Memory Game</h1>
+            <p>Click a Pokemon once.</p>
+            <p>Clicking the same pokemon resets the score</p>
+        </div>
+    );
+}
